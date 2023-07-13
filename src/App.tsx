cacfc127d82077f7e37082536017ef52fecd9727
De tarso oliveira araujo, Saulo [JANBR NON-J&J] <SdeTarso@its.jnj.com>
2023-07-13 16:19:25 -0300

    chore: Reduce fn

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,12 @@ import { PokemonsListing } from "./components";
 function App() {
   const [pokemons, setPokemons] = useState<PokemonInterface[]>([]);
   useEffect(() => {
-    (async () => {
-      const api = new PokemonClient();
-      const { results } = await api.listPokemons();
-      Promise.all(
-        results.map(({ name }) => new PokemonClient().getPokemonByName(name))
-      ).then((data) => setPokemons(data));
-    })();
+    const api = new PokemonClient();
+    api.listPokemons().then(({ results }) => {
+      Promise.all(results.map(({ name }) => api.getPokemonByName(name))).then(
+        (data) => setPokemons(data)
+      );
+    });
   }, []);
 
   return (
